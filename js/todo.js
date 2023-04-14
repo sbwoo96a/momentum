28b65dec3c05ac92b4fb57471d2e8cb7df6629b2
sbwoo96a <sbwoo96a@gmail.com>
2023-04-14 21:07:18 +0900

    Deleting To Dos part Three

--- js/todo.js
+++ js/todo.js
@@ -12,8 +12,10 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  console.log(li.id);
   li.remove();
+  console.log(typeof li.id);
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintTodo(newTodo) {
