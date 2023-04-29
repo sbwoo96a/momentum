79c26337c421c43655c64b62465d42bf06b3f4d2
sbwoo96a <sbwoo96a@gmail.com>
2023-04-29 20:21:47 +0900

    gh-pages

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,6 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form div input");
+const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -15,7 +15,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
